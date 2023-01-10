d0b0ed92ec1b7a05647a74edfb4cb077795839e3
af245 <camilofrnc182@gmail.com>
2023-01-10 11:15:17 -0500

    Filled info component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import './App.css'
+import Info from './components/Info'
 export default function App(){
     return(
         <div className='app--container'>
-        
+            <Info/>
         </div>
     )
 }
--- src/components/Info.js
+++ src/components/Info.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import picture from './picture.png'
+import './Info.css'
 
 export default function Info(){
     return(
-        <div>
-        
+        <div className='info--container'>
+            <div className='picture'>
+                <img src={picture}/>
+            </div>
+            <div className='description-container'>
+                <div className='name'>Laura Smith</div>
+                <div className='career'>Frontend Developer</div>
+                <div className='website'>laurasmith.website</div>
+            </div>
+            <div className='buttons-container'>
+                <button>Email</button>
+                <button>LinkedIn</button>
+            </div>
         </div>
     )
 }
