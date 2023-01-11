ede7bf7cfc9aeb79f04827445881f9c4952d1360
af245 <camilofrnc182@gmail.com>
2023-01-11 11:25:05 -0500

    added styling to info

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import picture from './picture.png'
 import './Info.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import {faLinkedin} from '@fortawesome/free-brands-svg-icons'
 
 export default function Info(){
     return(
@@ -14,8 +17,14 @@ export default function Info(){
                 <div className='website'>laurasmith.website</div>
             </div>
             <div className='buttons-container'>
-                <button>Email</button>
-                <button>LinkedIn</button>
+                <button className='btn--email'>
+                    <FontAwesomeIcon icon={faEnvelope}/>
+                    <span className='email'>Email</span>
+                </button>
+                <button className='btn--linkedIn'>
+                    <FontAwesomeIcon icon={faLinkedin}/>
+                    <span className='linkedIn'>linkedIn</span>
+                </button>
             </div>
         </div>
     )
